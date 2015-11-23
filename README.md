3174e30404a83d57a382eae67f2b8efe673c7e9b
braby21 <braby21@gmail.com>
2015-11-22 22:10:50 -0800

    git init

---
+++ README.md
@@ -0,0 +1,4 @@
+# willoerentals.com
+Capstone Project
+
+URL: http://braby21.github.io/willoerentals-com/
---
+++ wr.js
@@ -0,0 +1,155 @@
+$(function() {
+
+    var currentYear = new Date();
+    $('#footer-date').html("&copy;" + currentYear.getFullYear() + " Willoe Rentals");
+
+    var width = 576;
+    var animationSpeed = 2000;
+    var pause = 5000;
+    var currentSlide = 2;
+
+    //cache DOM
+    var $slider = $('#slider');
+    var $slides = $slider.find('.slides');
+    var $slide = $slides.find('.slide');
+    
+    var interval;
+
+    $('.slide-id').text(currentSlide);
+
+    function slideRight() {
+        $slides.animate({'margin-left': '-=' + width}, animationSpeed, function() {
+            currentSlide++;
+            $('.slide-id').text(currentSlide);
+            if (currentSlide == $slide.length)  {//if the current slide is the same as the length aka the last slide    
+                $('.slide-id').text(currentSlide);
+                currentSlide = 2;
+                $slides.css('margin-left',-576);
+            }
+        });
+    }
+
+    function slideLeft() {
+        $slides.animate({'margin-left': '+=' + width}, animationSpeed, function() {
+            currentSlide--;
+            if (currentSlide == 1)  {//if the current slide is the first slide
+                currentSlide = $slide.length - 1;
+                $slides.css('margin-left',-4032);
+            }
+        });
+    }
+
+    function startSlider() {
+        interval = setInterval(function() {
+            slideRight();
+        }, pause);  
+    }
+
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+    
+    $('.fa-arrow-circle-o-left').on('click', slideLeft);
+
+    $('.fa-arrow-circle-o-right').on('click', slideRight);
+
+    startSlider();
+
+    $slide.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
+
+    $('button').on('click', function(e){
+        e.preventDefault();
+        $('.form-container').css('visibility', 'hidden');
+        $('#overlay').css('z-index',-1);
+    });
+
+    $('#form-launch').on('click', function(e){
+        e.preventDefault();
+        $('.form-container').css('visibility', 'visible');
+        $('#name').val('');
+        $('#email').val('');
+        $('#phone').val('');
+        $('#comments').val('');
+        $('.map-container').css('visibility', 'hidden');
+
+        var docHeight = $(document).height();
+
+        $("body").append("<div id='overlay'></div>");
+
+        $("#overlay")
+        .height(docHeight)
+        .css({
+             'opacity' : 0.6,
+             'position': 'absolute',
+             'top': 0,
+             'left': 0,
+             'background-color': 'black',
+             'width': '100%',
+             'z-index': 1
+        });
+    });
+
+    $('#map-launch').on('click', function(e){
+        e.preventDefault();
+        $('.map-container').css('visibility', 'visible');
+        $('.form-container').css('visibility', 'hidden');
+
+        var docHeight = $(document).height();
+
+        $("body").append("<div id='overlay'></div>");
+
+        $("#overlay")
+        .height(docHeight)
+        .css({
+             'opacity' : 0.6,
+             'position': 'absolute',
+             'top': 0,
+             'left': 0,
+             'background-color': 'black',
+             'width': '100%',
+             'z-index': 1
+        });
+    });
+
+    $('body').on('click', function(e){
+
+        if(e.target.id == 'map-launch') {
+            e.preventDefault();
+            $('.map-container').css('visibility', 'visible');
+            $('.form-container').css('visibility', 'hidden');
+            pauseSlider();
+            return;
+        }
+
+        if(e.target.id == 'form-launch') {
+            e.preventDefault();
+            $('.form-container').css('visibility', 'visible');
+            $('#name').val('');
+            $('#email').val('');
+            $('#phone').val('');
+            $('#comments').val('');
+            $('.map-container').css('visibility', 'hidden');
+            pauseSlider();
+            return;
+        }
+
+        if ($('.map-container').css('visibility') == 'visible') {
+            $('.map-container').css('visibility', 'hidden');
+            $('#overlay').css('z-index',-1);
+            startSlider();
+        }
+
+        if ($('.form-container').css('visibility') == 'visible') {
+            $('.form-container').css('visibility', 'hidden');
+            $('#overlay').css('z-index',-1);
+            startSlider();
+        }
+
+    });
+
+
+
+
+
+
+});
