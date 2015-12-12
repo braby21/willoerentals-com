96d3f632271817809d0102823eecbc89d6671657
braby21 <braby21@gmail.com>
2015-12-12 10:48:43 -0800

    OOCSS & Fluid layouts

--- javascript/wr.js
+++ javascript/wr.js
@@ -1,17 +1,11 @@
 $(function() {
-
-    $('#stihl-button').on('mouseover', function(){
-        $(this).attr('src','images/stihl/en_button_small_a.png');
-    }).on('mouseout', function(){
-        $(this).attr('src','images/stihl/en_button_small_na.png');
-    });    
-
+  
     var currentYear = new Date();
     $('#footer-date').html("&copy;" + currentYear.getFullYear() + " Willoe Rentals");
 
     var width = 576;
     var animationSpeed = 2000;
-    var pause = 5000;
+    var pause = 4000;
     var currentSlide = 2;
 
     //cache the DOM
@@ -30,20 +24,20 @@ $(function() {
             if (currentSlide == $slide.length)  {//if the current slide is the same as the length aka the last slide    
                 $('.slide-id').text(currentSlide);
                 currentSlide = 2;
-                $slides.css('margin-left',-576);
+                $slides.css('margin-left',-680);
             }
         });
     }
 
-    function slideLeft() {
-        $slides.animate({'margin-left': '+=' + width}, animationSpeed, function() {
-            currentSlide--;
-            if (currentSlide == 1)  {//if the current slide is the first slide
-                currentSlide = $slide.length - 1;
-                $slides.css('margin-left',-4032);
-            }
-        });
-    }
+    // function slideLeft() {
+    //     $slides.animate({'margin-left': '+=' + width}, animationSpeed, function() {
+    //         currentSlide--;
+    //         if (currentSlide == 1)  {//if the current slide is the first slide
+    //             currentSlide = $slide.length - 1;
+    //             $slides.css('margin-left',-4032);
+    //         }
+    //     });
+    // }
 
     function startSlider() {
         interval = setInterval(function() {
@@ -59,31 +53,22 @@ $(function() {
 
     $slide.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
 
-    // #Launch Form
-    $('#form-launch').on('click', function(e){
-        if ($(window).width() < 720) {
-            return
-        } else {
-            e.preventDefault();
-            $('.form-container').css('display', 'block');
-            $('#name').val('');
-            $('#email').val('');
-            $('#phone').val('');
-            $('#inquiry').val('');
-            $('.overlay').css('display', 'block');
-            pauseSlider();
-        }
-    });
+    // // #Launch Form
+    // $('#form-launch').on('click', function(e){
+    //     if ($(window).width() < 720) {
+    //         return
+    //     } else {
+    //         e.preventDefault();
+    //         $('.form-container').css('display', 'block');
+    //         $('#name').val('');
+    //         $('#email').val('');
+    //         $('#phone').val('');
+    //         $('#inquiry').val('');
+    //         $('.overlay').css('display', 'block');
+    //         pauseSlider();
+    //     }
+    // });
 
-    // #Form-Close Button
-    $('.fa-times').on('click', function(e) { 
-        e.preventDefault();
-        $('.form-container').css('visibility', 'hidden');
-        $('.form-container').css('display', 'none');
-        $('.overlay').css('visibility', 'hidden');
-        $('.overlay').css('display', 'none');
-        startSlider();    
-    });
 
     // #Form-Submit Button
     $('#submit-form').on('click', function(e){
@@ -92,11 +77,6 @@ $(function() {
                   $('.missing-fields-message').css('visibility','visible');
                   return;
             }
-            $('.form-container').css('visibility', 'hidden');
-            $('.form-container').css('display', 'none');
-            $('.overlay').css('visibility', 'hidden');
-            $('.overlay').css('display', 'none');
-            startSlider();
     });
 
     // cache the DOM
@@ -125,7 +105,7 @@ $(function() {
         $rentalRatesTable.css('visibility','hidden');
     });
 
-    var mql = window.matchMedia("(min-width: 720px)");
+    var mql = window.matchMedia("(min-width: 768px)");
 
     var handleMediaChange = function (mediaQueryList) {
     
